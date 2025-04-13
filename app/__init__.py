71429cc679d5913f0c70dbeb1843b6d642df13b0
Georg von Gruben <georggruben@gmail.com>
2025-04-13 18:13:09 +0200

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+# app/__init__.py
+from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-key')
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
