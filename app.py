3c32eaa5c9972afeb5b7e23a8783d32c5cc3ff7b
Grubinio <georggruben@gmail.com>
2025-04-20 22:19:41 +0200

    Errorsites

--- app.py
+++ app.py
@@ -65,3 +65,11 @@ def antwort():
         #flash("Danke für deine Antwort!", "success")
         #return redirect(url_for('main.antwort'))
     return render_template('zusage.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
