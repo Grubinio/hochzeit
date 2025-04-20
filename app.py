f153aaedb438a7eaa1a4e924615802b0e1ad4968
Grubinio <georggruben@gmail.com>
2025-04-20 22:26:28 +0200

    Errorsites

--- app.py
+++ app.py
@@ -68,8 +68,8 @@ def antwort():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('4004.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template('500.html', error=e), 500
